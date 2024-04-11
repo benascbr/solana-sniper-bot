db91bc5df9b48e38af8e9a20b4d09d90ae472588
benaspaulikas <benas@paulikas.eu>
2024-04-11 11:47:01 +0300

    SOL support

--- buy.ts
+++ buy.ts
@@ -460,7 +460,7 @@ const runListener = async () => {
       const poolOpenTime = parseInt(poolState.poolOpenTime.toString());
       const existing = existingLiquidityPools.has(key);
 
-      if (true || (poolOpenTime > runTimestamp && !existing)) {
+      if (!existing || (poolOpenTime > runTimestamp && !existing)) {
         existingLiquidityPools.add(key);
         const _ = processRaydiumPool(updatedAccountInfo.accountId, poolState);
       }
